e1f7a5ec82611c658a283a24461685a51f1589af
XxmangreatxX <you@example.com>
2024-02-16 22:44:17 -0500

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const MoviesDB = require("./modules/moviesDB.js");
+const MoviesDB = require("./modules/MoviesDB.js");
 const path = require('path');
 
 require('dotenv').config();
